f4f4ff0a69a2d93b1a98badf7f6bbf0dcbb1146b
Holger von Ameln <holger.von.ameln@peercom.org>
2013-10-22 16:49:10 +0200

    add seding task to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,11 @@ namespace :deploy do
       # end
     end
   end
+  
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
 
   after :finishing, 'deploy:cleanup'
 
