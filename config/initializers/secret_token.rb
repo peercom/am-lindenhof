0cf45f75702fefe0871b28d3cd2b690f838d671c
Holger von Ameln <holger.von.ameln@peercom.org>
2013-10-22 10:01:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lindenhof::Application.config.secret_token = 'fdadafa820f0271b894017dcb98625986204eeb60f1b657f13ea88da3be2ca020cf2f5c00dbc4bc7a6db3087fe0f64dcaccef63b7c407d55f89a2eee42ed3194'
