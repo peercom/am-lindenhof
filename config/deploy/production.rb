f65280ff79c9295b9a887aed37e5b4e5759912ef
Holger von Ameln <holger.von.ameln@peercom.org>
2013-10-23 13:16:13 +0200

    configure production deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,21 +1,23 @@
 set :stage, :production
+set :rails_env, 'production'
 
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@am-lindenhof.de}
+role :web, %w{deploy@am-lindenhof.de}
+role :db,  %w{deploy@am-lindenhof.de}
 
+set :deploy_to, '/rails-apps/am-lindenhof.de_production'
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
 # definition into the server list. The second argument
 # something that quacks like a has can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
